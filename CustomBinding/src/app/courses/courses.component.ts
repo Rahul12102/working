c4fc146cc9ddc05295182cdcb2d05ff86723134a
Rahul Chhabra <rahul.chhabra@calsoftinc.com>
2023-12-13 13:16:11 +0530

    Pushing custom binding project

---
+++ CustomBinding/src/app/courses/courses.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+  courses = [
+
+    { id:101, name:'Javascript for beginners', author:'John Heikela', duration: 36, type: 'Free',
+    price: 0.00, ratings: 4.2, image: 'assets/jsforbeginners.jpg',
+    description: 'In this course you will learn the fundamentals of Javascript. This course is completely designed for beginners.'
+    },
+    { id:102, name:'Typescript', author:'Mark Vought', duration: 18, type: 'Free',
+    price: 0.00, ratings: 3.9, image: 'assets/ts.jpg',
+    description: 'In this course you will learn the fundamentals of Typescript. This course is completely designed for beginners.'
+    },
+    { id:103, name:'Angular for beginners', author:'Steve Smith', duration: 42, type: 'Premium',
+    price: 0.00, ratings: 4.0, image: 'assets/ang.jpg',
+    description: 'In this course you will learn the fundamentals of Angular. This course is completely designed for beginners.'
+    },
+    { id:104, name:' Advanced Javascript ', author:'John Heikela', duration: 45, type: 'Premium',
+    price: 0.00, ratings: 3.8, image: 'assets/ads.jpg',
+    description: 'In this course you will learn the fundamentals of Adv. Javascript. This course is completely designed for beginners.'
+    },
+    { id:105, name:'React for beginners', authorx :'Mark Vought', duration: 38, type: 'Free',
+    price: 0.00, ratings: 4.1, image: 'assets/react.jpg',
+    description: 'In this course you will learn the fundamentals of React. This course completely designed for beginners.'
+    },
+
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
