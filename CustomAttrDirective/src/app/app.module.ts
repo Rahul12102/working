03a4f66336441d7c70f8394fa0e2c7b5f8a47c8a
Rahul Chhabra <rahul.chhabra@calsoftinc.com>
2024-01-05 11:38:05 +0530

    CustomAttribute : Highlight

--- CustomAttrDirective/src/app/app.module.ts
+++ CustomAttrDirective/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SetBackgroundDirective } from './CustomDirective/setBackground.directive';
+import { HighlightDirective } from './CustomDirective/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SetBackgroundDirective
+    SetBackgroundDirective,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule
